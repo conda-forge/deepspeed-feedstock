91dfe45f9649225420b99a99cdc2acbb0cbb1a26
Wei Ji <23487320+weiji14@users.noreply.github.com>
2023-11-04 15:54:22 +1300

    Revert "Set DS_BUILD_RAGGED_DEVICE_OPS=0 flag"
    This reverts commit 32f2027a93f340dee3c25a3880f1e0674479d51c.

--- recipe/build.sh
+++ recipe/build.sh
@@ -29,7 +29,5 @@ fi
 export DS_BUILD_SPARSE_ATTN=0
 # Disable building with EvoFormerAttention which requires CUTLASS
 export DS_BUILD_EVOFORMER_ATTN=0
-# Disable building with ragged device ops
-export DS_BUILD_RAGGED_DEVICE_OPS=0
 
 ${PYTHON} -m pip install . -vv
