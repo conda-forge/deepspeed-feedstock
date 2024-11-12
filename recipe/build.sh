0f1ef5948765491586d41945ae13ff2db602eca4
Wei Ji <23487320+weiji14@users.noreply.github.com>
2024-11-12 21:42:42 +1300

    Recipe cleanup and re-enable some DS_BUILD_ ops (#74)
    * Remove if-branch for CUDA 11.2
    
    No more CUDA 11.2 builds on conda-forge since https://github.com/conda-forge/conda-forge-pinning-feedstock/issues/5339
    
    * Remove ninja as runtime dependency
    
    Xref #1
    
    * Bump build number to 1
    
    * Try building with EvoFormerAttention
    
    * Try building with CUTLASS ops
    
    * Try building with ragged device ops
    
    * MNT: Re-rendered with conda-build 24.9.0, conda-smithy 3.44.3, and conda-forge-pinning 2024.11.11.08.59.26

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,9 +8,7 @@ if [[ ${cuda_compiler_version} != "None" ]]; then
   # Set the CUDA arch list from
   # https://github.com/conda-forge/pytorch-cpu-feedstock/blob/86592106fc0e4731eb011ac763a4f0429326930b/recipe/build_pytorch.sh#L123
 
-  if [[ ${cuda_compiler_version} == 11.2 ]]; then
-    export TORCH_CUDA_ARCH_LIST="6.0;6.1;7.0;7.5;8.0;8.6+PTX"
-  elif [[ ${cuda_compiler_version} == 11.8 ]]; then
+  if [[ ${cuda_compiler_version} == 11.8 ]]; then
     export TORCH_CUDA_ARCH_LIST="6.0;6.1;7.0;7.5;8.0;8.6;8.9+PTX"
   elif [[ ${cuda_compiler_version} == 12.0 ]]; then
     export TORCH_CUDA_ARCH_LIST="6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
@@ -25,11 +23,5 @@ fi
 
 # Disable sparse_attn since it requires an exact version of triton==1.0.0
 export DS_BUILD_SPARSE_ATTN=0
-# Disable building with EvoFormerAttention which requires CUTLASS
-export DS_BUILD_EVOFORMER_ATTN=0
-# Disable building with CUTLASS ops
-export DS_BUILD_CUTLASS_OPS=0
-# Disable building with ragged device ops
-export DS_BUILD_RAGGED_DEVICE_OPS=0
 
 ${PYTHON} -m pip install . -vv
